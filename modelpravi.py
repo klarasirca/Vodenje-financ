fa7cd4cdbdb5bca60f9b88ac8b99c6ca657fdc4e
Klara Sirca <klara.sirca@gmail.com>
2019-09-02 14:35:51 +0200

    dodala funkcije za analitiko

--- modelpravi.py
+++ modelpravi.py
@@ -1,35 +1,39 @@
 import json
 import datetime
+from datetime import datetime as dt
 from enum import IntEnum
-from collections import namedtuple
 
-def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
-def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
-def obj2json(object): return json.dumps(object, default=lambda obj: obj.__dict__)
+def json2obj(data): 
+    obj_dict = json.loads(data)
+    return Racun(**obj_dict)
 
-class TipTransakcije(IntEnum):
-    ODHODEK = 0
-    DOHODEK = 1  
+def obj2json(object): 
+    return json.dumps(object, indent = 2, default = lambda obj: obj.__dict__)
+
+
+imena_mesecev = ["januar", "februar", "marec", "april", "maj", "junij", "julij", "avgust", "september", "oktober", "november", "december"]
+
+
 
 class Kategorija:
 
     seznam_kategorij_odhodek = ["Hrana", "Obleka", "Prosti čas", "Transport", 
         "Dom", "Položnice", "Zdravstvene storitve", "Potovanja"
-        , "Osebna nega", "Zavarovanja", "Najemnina"
-        , "Telekomunikacijske storitve", "Krediti", "Ostalo"]
+        , "Osebna nega", "Zavarovanja", "Najemnina", 
+        "Telekomunikacijske storitve", "Krediti", "Ostalo"]
 
     seznam_kategorij_dohodek = ["Plača", "Štipendija", "Honorar", "Posojilo", 
             "Žepnina", "Pokojnina", "Pasivni dohodki (najemnine itd.)",
             "Nagrada", "Ostalo"]
 
-    def getSeznamKategorij(self, tip: TipTransakcije):
-        if(tip==TipTransakcije.ODHODEK):
+    def getSeznamKategorij(self, tip):
+        if tip == 'Odhodek':
             return self.seznam_kategorij_odhodek
         else:
             return self.seznam_kategorij_dohodek
 
 class Transakcija:
-    def __init__(self, znesek, tip: TipTransakcije, kategorija, komentar):
+    def __init__(self, znesek, tip, kategorija, komentar):
         self.cas = datetime.datetime.now().timestamp()
         self.znesek = znesek 
         self.tip = tip
@@ -39,50 +43,65 @@ class Transakcija:
 
 class Racun:
 
-    def __init__(self, id, stanje=0, seznam_transakcij=[]):
+    def __init__(self, id, stanje = 0, seznam_transakcij = []):
         self.id = id
         self.stanje = stanje
-        self.seznam_transakcij: list = seznam_transakcij
+        self.seznam_transakcij = seznam_transakcij
 
     @staticmethod    
     def dodaj_transakcijo(racun, transakcija: Transakcija):
         racun.seznam_transakcij.append(transakcija)
-        
-        if(transakcija.tip==TipTransakcije.ODHODEK):
-            racun.stanje-=transakcija.znesek
+        if(transakcija.tip == 'odhodek'):
+            racun.stanje -= transakcija.znesek
         else:
-            racun.stanje+=transakcija.znesek
+            racun.stanje += transakcija.znesek
             
-    
-    #funkcije za analitiko
-    
-     def koliko_zapravljeno_kategorija(self, obdobje_mesec, obdobje_leto):
-        slovar_kategorija = {"Skupaj odhodki": 0, "Skupaj prihodki": 0}
-        for transakcija in self.seznam_transakcij:
-            cas_normalno = dt.fromtimestamp(transakcija[0])
-            leto = cas_normalno.year
-            mesec = cas_normalno.month
-            if mesec == obdobje_mesec and leto == obdobje_leto:
-                kategorija_st = transakcija[3]
-                tip = transakcija[2]
-                sez_kat = Kategorija().getSeznamKategorij(tip)
-                kat = sez_kat[kategorija_st]
-                slovar_kategorija[kat] = slovar_kategorija.get(kat, 0) + transakcija[1]
-                if tip == 0:
-                    slovar_kategorija["Skupaj odhodki"] = slovar_kategorija.get("Skupaj odhodki", 0) + transakcija[1]
-                else:
-                    slovar_kategorija["Skupaj prihodki"] = slovar_kategorija.get("Skupaj prihodki", 0) + transakcija[1]
-        return slovar_kategorija 
-        #predelati potrebno po času
+
 
 def shraniRacun(racun: Racun):
     jsonRacun = obj2json(racun)
-    with open("racun_"+str(racun.id)+".json", "w+") as file:
+    with open("racun_" + str(racun.id) + ".json", "w+") as file:
         file.write(jsonRacun)
 
-def naloziRacun(racunId: int):
+def naloziRacun(racunId):
     jsonRacun = ""
-    with open("racun_"+str(racunId)+".json", "r") as file:
-        jsonRacun=file.read()
-    obj_racun=json2obj(jsonRacun)
-    return Racun(obj_racun.id, obj_racun.stanje, obj_racun.seznam_transakcij)
+    with open("racun_" + str(racunId) + ".json", "r") as file:
+        jsonRacun = file.read()
+    obj_racun = json2obj(jsonRacun)
+    return obj_racun
+
+#funkcije za analitiko:
+
+def ali_je_transakcija_pravi_cas(transakcija: Transakcija, obdobje_mesec, obdobje_leto):
+    cas_normalno = dt.fromtimestamp(transakcija["cas"])
+    leto = cas_normalno.year
+    mesec = cas_normalno.month
+    return mesec == obdobje_mesec and leto == obdobje_leto
+
+
+def koliko_kategorija(racun: Racun, tip, obdobje_mesec, obdobje_leto):
+        slovar_kategorija = {}
+        for transakcija in racun.seznam_transakcij:
+            if ali_je_transakcija_pravi_cas(transakcija, obdobje_mesec, obdobje_leto) and transakcija["tip"] == tip:
+                kat = transakcija['kategorija']
+                slovar_kategorija[kat] = slovar_kategorija.get(kat, 0) + transakcija["znesek"]
+        return slovar_kategorija
+
+
+def skupaj_prihodki_odhodki(racun: Racun, tip, obdobje_mesec, obdobje_leto):
+    slovar_kategorij = koliko_kategorija(racun, tip, obdobje_mesec, obdobje_leto)
+    skupaj_tip = 0
+    for kategorija in slovar_kategorij:
+        znesek = slovar_kategorij[kategorija]
+        skupaj_tip += znesek
+    return skupaj_tip
+
+def procentualno_kategorija(racun: Racun, tip, obdobje_mesec, obdobje_leto):
+    slovar_kategorij = koliko_kategorija(racun, tip, obdobje_mesec, obdobje_leto)
+    skupaj = skupaj_prihodki_odhodki(racun, tip, obdobje_mesec, obdobje_leto)
+    slovar_procentov = {}
+    for kategorija in slovar_kategorij:
+        koliko_kat = slovar_kategorij[kategorija]
+        procent = round ((koliko_kat / skupaj) * 100, 1)
+        slovar_procentov[kategorija] = procent
+    return slovar_procentov
