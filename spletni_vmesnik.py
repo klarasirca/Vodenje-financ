bff5ad2dc49d9fba1f61fbb29312dd33a771d97c
Klara Sirca <klara.sirca@gmail.com>
2019-09-07 01:20:05 +0200

    uredila imena spremenljivk

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -26,7 +26,6 @@ def registriran():
     ime_priimek = bottle.request.forms.ime_priimek
     bottle.response.set_cookie("ime_priimek",ime_priimek, path="/")
     racunid = randint(10000, 20000)
-    print(str(racunid))
     bottle.response.set_cookie("racunid",str(racunid), path="/")
     ustvari_racun(racunid, ime_priimek) 
     return bottle.redirect('/registriran/')
@@ -34,28 +33,28 @@ def registriran():
 @bottle.get('/registriran/')
 def registriran_rezultat():
     return bottle.redirect('/domaca_stran/')
-            
-@bottle.get('/<tip>')
-def dodaj_trans_tip(tip):
-    return bottle.template('dodaj_trans.tpl', tip=tip, seznamK=Kategorija().getSeznamKategorij(tip))
 
 @bottle.get('/domaca_stran/')
 def domaca_stran():
     ime_priimek = bottle.request.get_cookie("ime_priimek")
     racunid = bottle.request.get_cookie("racunid")
-    racun = naloziRacun(racunid)
+    racun = nalozi_racun(racunid)
     print(racunid)
-    if racun is None:
+    if racun == None:
         return bottle.template('zacetna_stran.tpl', login = False)
     ime_priimek = racun.lastnik
     meseci = imena_mesecev
-    return bottle.template('domaca_stran.tpl', seznamT=racun.seznam_transakcij, total=racun.stanje, meseci = meseci, ime_priimek = ime_priimek, racunid = racunid)
+    return bottle.template('domaca_stran.tpl', total = racun.stanje, meseci = meseci, ime_priimek = ime_priimek, racunid = racunid)
 
 @bottle.get('/domaca_stran/odjava/')
 def odjavi_me():
-    bottle.response.delete_cookie("racunid", path="/")
-    bottle.response.delete_cookie("ime_priimek", path="/")
+    bottle.response.delete_cookie("racunid", path = "/")
+    bottle.response.delete_cookie("ime_priimek", path = "/")
     return bottle.redirect("/")
+
+@bottle.get('/<tip>')
+def dodaj_trans_tip(tip):
+    return bottle.template('dodaj_trans.tpl', tip=tip, seznam_kategorij = Kategorija().pridobi_seznam_kategorij(tip))
 
 @bottle.post('/dodaj_transakcijo')
 def dodaj_transakcijo():
@@ -65,9 +64,9 @@ def dodaj_transakcijo():
     kategorija = bottle.request.forms.kategorija
     komentar = bottle.request.forms.komentar
     trans = Transakcija(znesek,tip,kategorija,komentar)
-    racun=naloziRacun(racunid)
+    racun = nalozi_racun(racunid)
     racun.dodaj_transakcijo(trans)
-    shraniRacun(racun)
+    shrani_racun(racun)
     bottle.redirect("/dodaj_transakcijo")
 
 @bottle.get('/dodaj_transakcijo')
@@ -76,12 +75,12 @@ def dodaj_transakcijo_rezultat():
 
 @bottle.get('/pridobi_kategorije/<tip>')
 def pridobi_kategorije(tip):
-    return obj2json(Kategorija().getSeznamKategorij(tip))
+    return obj2json(Kategorija().pridobi_seznam_kategorij(tip))
 
 @bottle.get('/analiza_podatkov/')
 def analiza_podatkov():
     racunid = int(bottle.request.get_cookie("racunid"))
-    racun = naloziRacun(racunid)
+    racun = nalozi_racun(racunid)
     mesec = int(bottle.request.query["mesec"])
     leto = int(bottle.request.query["leto"])
     slovar_zapravljeno = koliko_kategorija(racun, "Odhodek", leto, mesec)
@@ -97,24 +96,24 @@ def analiza_podatkov():
    
 @bottle.get('/seznam_transakcij/')
 def get_seznam_transakcij():
-    racun=naloziRacun(int(bottle.request.get_cookie("racunid")))
-    seznamT = racun.seznam_transakcij
+    racun=nalozi_racun(int(bottle.request.get_cookie("racunid")))
+    seznam_transakcij = racun.seznam_transakcij
     cas = date.today().strftime("%d-%m-%y")
-    return bottle.template('transakcije.tpl', seznamT = seznamT, cas=cas)
+    return bottle.template('transakcije.tpl', seznam_transakcij = seznam_transakcij, cas=cas)
 
 @bottle.get('/opozorilo/')
 def opozorilo_pridobi_kategorijo():
-    seznamK = Kategorija().getSeznamKategorij("Odhodek")
-    return bottle.template('nastavi_opozorilo.tpl', seznamK = seznamK)
+    seznam_kategorij = Kategorija().pridobi_seznam_kategorij("Odhodek")
+    return bottle.template('nastavi_opozorilo.tpl', seznam_kategorij = seznam_kategorij)
 
 
 @bottle.post('/nastavi_opozorilo/')
 def nastavi_opozorilo_v_slovar():
     limit = bottle.request.forms["limit"]
     kategorija_limit = bottle.request.forms.kategorija_limit
-    racun = naloziRacun(int(bottle.request.get_cookie("racunid")))
+    racun = nalozi_racun(int(bottle.request.get_cookie("racunid")))
     racun.nastavi_opozorilo(limit, kategorija_limit)
-    shraniRacun(racun)
+    shrani_racun(racun)
     return bottle.redirect('/nastavi_opozorilo/')
 
 @bottle.get('/nastavi_opozorilo/')
@@ -122,35 +121,29 @@ def opozorilo_rezultat():
    return bottle.template('uspesna_nastavitev_odstranitev.tpl', nastavi = True)
 
 @bottle.get('/opozorilo/odstrani_opozorilo/')
-def pridobi_kategorije():
-    seznamK = Kategorija().getSeznamKategorij("Odhodek")
-    racun = naloziRacun(int(bottle.request.get_cookie("racunid")))
+def pridobi_kategorije_opozorilo():
+    seznam_kategorij = Kategorija().pridobi_seznam_kategorij("Odhodek")
+    racun = nalozi_racun(int(bottle.request.get_cookie("racunid")))
     opozorila_slovar = racun.slovar_opozoril
     seznam_kategorij_opozorila = []
     for kategorija in opozorila_slovar.keys():
         seznam_kategorij_opozorila.append(kategorija)
-    seznam_kategorij_neopozorila = [kategorija for kategorija in seznamK if kategorija not in seznam_kategorij_opozorila]
+    seznam_kategorij_neopozorila = [kategorija for kategorija in seznam_kategorij if kategorija not in seznam_kategorij_opozorila]
     return bottle.template('odstrani_opozorilo.tpl', seznam_kategorij_opozorila = seznam_kategorij_opozorila, seznam_kategorij_neopozorila = seznam_kategorij_neopozorila)
 
 @bottle.post('/odstranjeno/')
 def odstrani():
     kategorija_limit = bottle.request.forms.kategorija_limit
-    racun = naloziRacun(int(bottle.request.get_cookie("racunid")))
-    if kategorija_limit in racun.slovar_opozoril:
-        racun.odstrani_opozorilo(kategorija_limit)
-        shraniRacun(racun)
-        return bottle.redirect('/odstranjeno/')
-    else:
-        return bottle.template('uspesna_nastavitev_odstranitev', nastavi = False)
+    racun = nalozi_racun(int(bottle.request.get_cookie("racunid")))
+    racun.odstrani_opozorilo(kategorija_limit)
+    shrani_racun(racun)
+    return bottle.redirect('/odstranjeno/')
         
     
 @bottle.get('/odstranjeno/')
 def odstrani_rezultat():
     return bottle.template('uspesna_nastavitev_odstranitev', nastavi = False)
 
-
-
-    
 
 @bottle.get('/img/<ime>')
 def slike(ime):
